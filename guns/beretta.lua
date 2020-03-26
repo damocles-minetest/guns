7ed0406ba9d32142d99bafe013cd297b476be5c0
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-03-26 11:11:32 +0100

    beretta intervall

--- guns/beretta.lua
+++ guns/beretta.lua
@@ -1,14 +1,14 @@
 
 
 guns.register_gun({
-  interval = 0.25,
+  interval = 0.09,
   name = "guns:beretta",
   description = "Beretta",
   texture = "guns_beretta.png",
   ammo = "guns:ammo_9mm",
   ammo_count = 25,
   sound = "guns_beretta",
-  damage = 5,
+  damage = 2,
   range = 50,
   on_hit = guns.smoke
 })
